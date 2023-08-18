b3b5cec80bd04d949c1a3eb404e9ce1ddbd5e7d0
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-18 10:28:41 -0500

    Add data attributes and click event

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -2,15 +2,26 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const whatWasClicked = clickEvent.target
+        
+        if (whatWasClicked.dataset.type === "walker") {
+            window.alert(`This walker works in ${whatWasClicked.dataset.city}`)
+        }
+    }
+)
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
         walkerHTML += `<li data-id="${walker.id}"
-                           data-city="${walker.city}
-                >${walker.name}
-            </li>`
+                           data-city="${walker.city}"
+                           data-type="walker"
+                           >${walker.name}
+                       </li>`
     }
 
     walkerHTML += "</ul>"
